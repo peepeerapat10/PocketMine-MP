0620cf3d68c9279d4c497ebc0441984a359ea140
peepeerapat10 <minecraftpee567@gmail.com>
2022-03-19 21:13:50 +0700

    Delete bin/php7/lib directory

--- bin/php7/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/runner/work/php-build-scripts/php-build-scripts/bin/php7/lib"
-XML2_LIBS="-lxml2 -L/home/runner/work/php-build-scripts/php-build-scripts/bin/php7/lib -lz     -lm "
-XML2_INCLUDEDIR="-I/home/runner/work/php-build-scripts/php-build-scripts/bin/php7/include/libxml2"
-MODULE_VERSION="xml2-2.9.12"
-
